b7b76976bcea8828137d530687453a28cc856fd7
Dmitrii Nazarov <dimanidfifa15@gmail.com>
2023-10-25 19:22:37 +0300

    Tdd2 tests

---
+++ TDD2.py
@@ -0,0 +1,17 @@
+from pip._internal.utils.misc import capturedoutput
+
+from durak import текущаяситуация, основнаяигра
+
+
+def testвыводситуации(игроки):
+    текущаяситуация(игроки)
+
+    ожидаемый_вывод = "Игрок 1: 6 карт\nИгрок 2: 6 карт"
+
+    assert capturedoutput() == ожидаемыйвывод
+
+
+def testосновнаяигра():
+    основная_игра(2)
+
+    assert "Игра завершена" in captured_output()
